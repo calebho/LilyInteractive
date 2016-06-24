eaa907504d01ad61e749344c3f133b3863975017
Caleb <caleb.ho@me.com>
2016-06-24 15:08:27 -0400

    cleanup getInputString WIP

--- speech_recog.py
+++ speech_recog.py
@@ -19,14 +19,12 @@ def getInputString():
     while not s:
         # use the default microphone as the audio source
         with sr.Microphone() as source:
-            audio = r.adjust_for_ambient_noise(source, duration = 0.5)
+            r.adjust_for_ambient_noise(source, duration = 0.5)
             print "listening..."
-            # listen for the first phrase and extract it into audio dataprint "heard"
             audio = r.listen(source)
         try:
-            all_list = r.recognize(audio)
-            s = ''+all_list
-            # recognize speech using Google Speech Recognition
+            all_list = r.recognize_google(audio)
+            s = all_list
         except LookupError: # speech is unintelligible
             speak("Sorry, I didn't hear that.")
             s = None
