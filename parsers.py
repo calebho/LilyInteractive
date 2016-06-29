941786b56cee8c437d34b37403d6766ec3f7a50f
Caleb <caleb.ho@me.com>
2016-06-29 16:29:21 -0400

    accept multiple example sentences

--- parsers.py
+++ parsers.py
@@ -6,11 +6,15 @@ from spacy.attrs import TAG
 NLP = spacy.en.English()
 NLP.matcher.add("ItemChoice", "PRODUCT", {}, # arbitrary attributes ???
     [ # patterns
-        [ # i.e. some chocolate
-            {TAG: "DT"},
+        [ # i.e. popcorn
             {TAG: "NN"}
         ],
-        [ # i.e. popcorn
+        [ # i.e. ice cream
+            {TAG: "NN"},
+            {TAG: "NN"},
+        ],
+        [ # i.e. some chocolate
+            {TAG: "DT"},
             {TAG: "NN"}
         ],
         [ # i.e. a cup of soda
@@ -19,41 +23,50 @@ NLP.matcher.add("ItemChoice", "PRODUCT", {}, # arbitrary attributes ???
             {TAG: "IN"},
             {TAG: "NN"}
         ]
+        # TODO: a large cup of soda, ice cream in a cone, etc.
     ]
 )
 
-def parse_choices(s, example):
-    """Given a sentence `s` of user choices and an example sentence `example`,
-    parse and return a list of the choices
+def parse_choices(s, examples):
+    """Given a sentence `s` of user choices and a list of example sentences
+    `examples`, parse and return a list of the choices
     """
-    sim_threshold = 0.6
+    sim_threshold = 0.7
     user_sent = NLP(unicode(s))
-    ex_sent = NLP(unicode(example))
+    examples = [NLP(unicode(example)) for example in examples]
+    ex_sent = max(examples, key=lambda s: s.similarity(user_sent))
     if user_sent.similarity(ex_sent) < sim_threshold:
-        raise RuntimeError("The sentences\n%s\n%s\naren't similiar enough" % \
-            (user_sent, ex_sent))
+        raise RuntimeError(
+            "\nThe sentences\n\t%s\n\t%s\naren't similiar enough\n" % \
+            (user_sent, ex_sent) + \
+            "similarity score: %.4f" % user_sent.similarity(ex_sent))
 
     choices = []
-    for ent in user_sent:
-        if len(ent) == 1: # just a noun
-            choices.append(ent.text)
-        else: # get the base noun
-            noun_phrase = NLP(ent.text)
-            chunk = noun_phrase.noun_chunks
-            assert len(chunk) == 1
-            choices.append(chunk.text)
+    for ent in user_sent.ents:
+        choices.append(ent.text)
 
-    return choices 
+    return choices
 
 def main():
-    test_sents = [u'I want popcorn and soda',
-                  u'Popcorn, candy, and chocolate',
+    food_sents = [u'I want popcorn and soda',
+                  u'popcorn, candy, and chocolate',
                   u"I'd like some soda and a cup of coffee"]
+    examples = ["I want an apple and an orange",
+                "A glass of water and some chips"]
 
-    for test_sent in test_sents:
-        doc = NLP(test_sent)
-        for ent in doc.ents:
-            print ent.text, ent.label_, [w.tag_ for w in ent]
+    for test_sent in food_sents:
+        print parse_choices(test_sent, examples)
+
+    movie_sents = [u'I want to watch captain america',
+                   u"I'd like a ticket for spiderman",
+                   u'Up, please'] # TODO: fails 
+    examples = ["I want to watch Minions",
+                "Tomorrowland, please",
+                "Inside Out"]
+
+    for test_sent in movie_sents:
+        print parse_choices(test_sent, examples)
+
 
 if __name__ == '__main__':
     main()
