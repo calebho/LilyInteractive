58f50018986050a76c2ccd5f89720da58f04e111
Caleb <caleb.ho@me.com>
2016-06-29 17:20:39 -0400

    new text_to_speech start

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,8 +1,10 @@
+
+"""
 import pyttsx
 import win32con, win32api
 
 
-'''This file handles the text to speech aspect of the interactive story using the pyttsx module to 
+'''This file handles the text to speech aspect of the interactive story using the pyttsx module to
 produce the speech. Additionally, we use the win32api module for python, which encapsulates the Windows win32api, allowing
 us to send a click to our avatar. Each click changes the current avatar animation being run, simulating a talking or idle state. '''
 
@@ -45,9 +47,8 @@ def click(x,y):
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
 
-       
+
 #IN THE EVENT BALDISYNC IS USED REPLACE SPEAK WITH BELOW CODE
 #lib.typeInBaldi(string)
 #time.sleep(2*math.log10(len(string)) + 1)
-
-
+"""
