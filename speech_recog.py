8e85a4e9772ed4add8eaec5ffcf2ec8bb3419c94
Caleb Ho <caleb.ho@me.com>
2016-07-14 16:47:39 -0400

    use Watson STT for speech recognition NOT TESTED YET

--- speech_recog.py
+++ speech_recog.py
@@ -1,48 +1,42 @@
 import speech_recognition as sr
+
+from watson_developer_cloud import SpeechToTextV1
 from text_to_speech import speak
 
+# Watson speech to text service credentials
+UNAME = '68cf9a6e-63a2-404a-9c96-295de6aac15d'
+PWD = 'D8MFSdf2TsWS'
+
+stt = SpeechToTextV1(username=UNAME, password=PWD)
 
 r = sr.Recognizer()
 r.pause_threshold = 1
 r.energy_threshold = 2200
 
-def getInputString():
+def get_input(): # TODO: add keyword recognition?
+    """Using audio data recorded from the microphone, do speech to text on the
+    audio and return the transcription
     """
-    """
+    # FOR DEBUGGING 
+    # return raw_input('input: ')
+    
+    t = 0.8 # confidence threshold
     s = None
     while not s:
         # use the default microphone as the audio source
         with sr.Microphone() as source:
             r.adjust_for_ambient_noise(source, duration = 0.5)
-            print "listening..."
-            audio = r.listen(source)
-        try:
-            all_list = r.recognize_google(audio)
-            s = all_list
-        except LookupError: # speech is unintelligible
-            speak("Sorry, I didn't hear that.")
-            s = None
-    print(s)
-    return str(s)
+            # print "listening..."
+            audio = r.listen(source).get_wav_data()
+        response = stt.recognize(audio, content_type='audio/wav',
+                model='en-US_BroadbandModel')
+        best_transcript = response['alternatives'][0]
+        s = best_transcript['transcript'] if best_transcript['confidence'] > t else None
+    
+    return s
 
+def main():
+    pass 
 
-def getInputStringMultiple():
-    s = None
-    while not s:
-        # use the default microphone as the audio source
-        with sr.Microphone() as source:
-            audio = r.adjust_for_ambient_noise(source, duration = 0.5)
-            print "listening..."
-            # listen for the first phrase and extract it into audio dataprint "heard"
-            audio = r.listen(source)
-        try:
-            all_list = r.recognize(audio, show_all = True)
-            s = ''
-            for item in all_list:
-                s +=item['text']+' '
-            # recognize speech using Google Speech Recognition
-        except LookupError: # speech is unintelligible
-            speak("Sorry, I didn't hear that.")
-            s = None
-    print(s)
-    return str(s)
+if __name__ == '__main__':
+    main()
