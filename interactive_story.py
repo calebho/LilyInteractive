f2db2d320ff94947b82c7da5db616fac7c3220cb
Caleb Ho <caleb.ho@me.com>
2016-07-15 11:48:27 -0400

    use Watson to do NL interface

--- interactive_story.py
+++ interactive_story.py
@@ -1,6 +1,11 @@
 from movie_story import movie_story_factory
 from zoo_story import zoo_story_factory
 from story import StoryError
+from speech_recog import get_input
+from parsers import parse, get_intent
+from text_to_speech import speak, wrap_text
+
+WKSPACE_ID = "34b49656-59ce-4f40-8c32-cc8bb846f8cd"
 
 def get_name():
     """Gets the user's name
@@ -11,13 +16,24 @@ def get_story():
     """Gets a story choice from the user and returns the appropriate Story object
     and player dict
     """
-    story_choice = raw_input('which story? ').strip()
-    if story_choice == 'movie':
-        s = movie_story_factory()
-    elif story_choice == 'zoo':
-        s = zoo_story_factory()
-    elif story_choice == 'pet store':
-        pass
+    msg = "Where would you like to go today? We can go to the zoo to see " + \
+          "some animals, or we can go to the movies and watch a film."
+    speak(wrap_text(msg, 'GoodNews'))
+    s = None
+    while not s:
+        inp = get_input()
+        resp = parse(inp, WKSPACE_ID)
+        intent = get_intent(resp)
+        if intent == 'movie':
+            s = movie_story_factory()
+        elif intent == 'zoo':
+            s = zoo_story_factory()
+        elif intent == 'pet store':
+            pass
+        else:
+            msg = "I'm sorry, I didn't understand what you said. Could you " + \
+                  "try repeating yourself or rephrasing?"
+            speak(wrap_text(msg, 'Apology'))
 
     return s
 
