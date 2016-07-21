ce7f0352c2d25f6955e7916c2b41f51ca89d8f69
Caleb Ho <caleb.ho@me.com>
2016-07-21 14:13:51 -0400

    basic add node functionality in edit screen

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
+from __future__ import print_function
+
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, FallOutTransition
+from kivy.uix.bubble import Bubble
+from kivy.uix.label import Label
+from kivy.uix.scatter import Scatter
 
 Builder.load_file("storyboard.kv")
+
+class edit_menu(Bubble):
+    pass
+
+class StoryLabel(Label):
+    pass
+
+class StoryNode(Scatter):
+    pass
+
+class Board(FloatLayout):
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            # check whether we've collided with a widget
+            result = super(Board, self).on_touch_down(touch)
+            # if not, add a node and stop the touch event propagation
+            if not result:
+                n = StoryNode(do_scale=False, do_rotation=False)
+                n.center = touch.pos
+                self.add_widget(n)
+                return True
 
 class HomeScreen(Screen):
     pass
