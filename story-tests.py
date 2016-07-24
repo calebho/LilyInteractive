61827723f3b5a295236ed6b4ab8ca94a19a5e7ed
Caleb Ho <caleb.ho@me.com>
2016-07-24 15:58:18 -0400

    rewrite add edge tests

--- story-tests.py
+++ story-tests.py
@@ -27,6 +27,44 @@ class StoryTests(unittest.TestCase):
         self.assertTrue('a' in self.s)
         self.assertTrue('1' in self.s)
 
+    def test_add_edge(self):
+        self.s.add_node('a')
+        self.s.add_node('b')
+        self.s.add_edge('a', 'b')
+        self.assertTrue('a' in self.s)
+        self.assertTrue('b' in self.s)
+        self.assertTrue('b' in self.s.neighbors('a'))
+        self.assertFalse('a' in self.s.neighbors('b'))
+
+        self.s.add_edge('c', 'd')
+        self.assertTrue('c' in self.s)
+        self.assertTrue('d' in self.s)
+        self.assertTrue('d' in self.s.neighbors('c'))
+        self.assertFalse('c' in self.s.neighbors('d'))
+
+        ebunch = [('b', 'c'), ('d', 'e')]
+        self.s.add_edges_from(ebunch)
+        self.assertTrue('e' in self.s)
+        
+    def test_add_undir_edge(self):
+        self.s.add_node('a')
+        self.s.add_node('b')
+        self.s.add_undirected_edge('a', 'b')
+        self.assertTrue('a' in self.s)
+        self.assertTrue('b' in self.s)
+        self.assertTrue('b' in self.s.neighbors('a'))
+        self.assertTrue('a' in self.s.neighbors('b'))
+
+        self.s.add_undirected_edge('c', 'd')
+        self.assertTrue('c' in self.s)
+        self.assertTrue('d' in self.s)
+        self.assertTrue('d' in self.s.neighbors('c'))
+        self.assertTrue('c' in self.s.neighbors('d'))
+
+        ebunch = [('b', 'c'), ('d', 'e')]
+        self.s.add_undirected_edges_from(ebunch)
+        self.assertTrue('e' in self.s)
+
     '''
     def test_context(self):
         d = {'a': 1, 'b': 2}
@@ -34,59 +72,6 @@ class StoryTests(unittest.TestCase):
         self.assertTrue(self.s.context == d)
         d['c'] = 3 # should not change s.context
         self.assertFalse(self.s.context == d)
-
-    def test_add_edge(self):
-        a = 'not callable'
-        b = 'also not callable'
-        with self.assertRaises(AssertionError):
-            self.s.add_edge(a, b)
-        
-        f1 = lambda: None
-        f2 = lambda: None 
-        self.s.add_edge(f1, f2)
-        self.assertTrue(f1 in self.s)
-        self.assertTrue(f2 in self.s)
-        self.assertTrue(f2 in self.s.neighbors(f1))
-        self.assertFalse(f1 in self.s.neighbors(f2))
-        
-        g1 = lambda: None 
-        g2 = lambda: None 
-        h1 = lambda: None 
-        h2 = lambda: None 
-        ebunch = [(g1, g2), (h1, h2)]
-        self.s.add_edges_from(ebunch)
-        for fct1, fct2 in ebunch:
-            self.assertTrue(fct1 in self.s)
-            self.assertTrue(fct2 in self.s)
-            self.assertTrue(fct2 in self.s.neighbors(fct1))
-            self.assertFalse(fct1 in self.s.neighbors(fct2))
-
-    def test_add_undir_edge(self):
-        a = 'not callable'
-        b = 'also not callable'
-        with self.assertRaises(AssertionError):
-            self.s.add_undirected_edge(a, b)
-        
-        f1 = lambda: None
-        f2 = lambda: None 
-        self.s.add_undirected_edge(f1, f2)
-        self.assertTrue(f1 in self.s)
-        self.assertTrue(f2 in self.s)
-        self.assertTrue(f1 in self.s and f2 in self.s)
-        self.assertTrue(f2 in self.s.neighbors(f1))
-        self.assertTrue(f1 in self.s.neighbors(f2))
-        
-        g1 = lambda: None 
-        g2 = lambda: None 
-        h1 = lambda: None 
-        h2 = lambda: None 
-        ebunch = [(g1, g2), (h1, h2)]
-        self.s.add_undirected_edges_from(ebunch)
-        for fct1, fct2 in ebunch:
-            self.assertTrue(fct1 in self.s)
-            self.assertTrue(fct2 in self.s)
-            self.assertTrue(fct2 in self.s.neighbors(fct1))
-            self.assertTrue(fct1 in self.s.neighbors(fct2))
 
 
     def test_run(self):
